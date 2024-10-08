f10dd916eda1589c5f2688cbfd7e480fdb5ad67f
projecthyperion <163935207+projecthyperion@users.noreply.github.com>
2024-10-08 19:57:10 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+# outputs.tf
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
